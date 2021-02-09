124e637b37e87bc3bc1e3733d233bb816aeee9ba
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2021-02-09 15:21:30 +0530

    serverless function undefined handled

--- pages/api/singleTweet.js
+++ pages/api/singleTweet.js
@@ -8,9 +8,10 @@ module.exports = async(req, res) => {
         access_token_key: process.env.acessToken,
         access_token_secret: process.env.tokenSecret
     });
+    const message = req.body.body.message;
     const shootPromise = () => {
         return new Promise((resolve, reject) => {
-            client.post('statuses/update.json', { status: req.body.body.message } , (error, data) => {
+            client.post('statuses/update.json', { status: message } , (error, data) => {
                 if(error) reject(error)
                 resolve(data)       
             });
