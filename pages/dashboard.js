cd7c4d46a6ccd977af1ff32ed7b82d718fef5a8f
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2021-02-13 10:10:40 +0530

    changes in dashboard url routing

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,17 @@
 import { Dashboard } from '../components';
-import { useRouter } from 'next/router';
-import { useSelector } from 'react-redux';
+import Cookies from 'next-cookies';
 
 export default function DashbaordPage(){ 
     return <Dashboard />
 };
+
+export async function getServerSideProps({ req, res}){
+    const token = Cookies({req, res});
+    if(token.uid === undefined){
+        res.statusCode = 302
+        res.setHeader('Location', `/login`) 
+        return { props: { }};
+    }else {
+        return { props: { } }
+    }
+}
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
+import "firebase/storage";
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
